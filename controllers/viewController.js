32db481c3830a7fc846ee81517fd84aa08d44db9
santio89 <santiolais1989@gmail.com>
2022-06-24 17:12:24 -0300

    adding multer

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,5 +1,17 @@
 const contenedor = require("./apiController").contenedorProductos;
 const items = contenedor.productos;
+const multer = require("multer");
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, "public/uploads")
+    },
+    filename: function (req, file, cb){
+        file.date = Date.now();
+        cb(null, file.date + "-" + file.originalname )
+    }
+})
+const upload = multer({storage: storage})
+
 
 const formView = (req, res)=>{
     res.render("form")
@@ -9,9 +21,15 @@ const productsView = (req, res)=>{
     res.render("productos", {items})
 }
 
+
 const formSent = (req, res)=>{
+    const img = req.body.formImage
+    
+    if (img == "file" && req.file){
+        req.body.thumbnail = `/uploads/${req.file.date}-${req.file.originalname}`
+    } 
     contenedor.save(req.body)
     res.redirect("/")
 }
 
-module.exports={formView, productsView, formSent}
+module.exports={formView, productsView, formSent, upload}
--- routes/indexViewRoutes.js
+++ routes/indexViewRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const router = express.Router();
-const {formView, productsView, formSent} = require("../controllers/viewController");
+const {formView, productsView, upload, formSent} = require("../controllers/viewController");
 
 /* ruteo */
 router.get("/", formView)
 router.get("/productos", productsView)
-router.post("/productos", formSent)
+router.post("/productos", upload.single("imgFile"), formSent)
 
 
 module.exports = {router};
